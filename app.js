ee627708ed3524896f4d78e4b4d0abb974dbf5b1
yuasa <y.kyuri2014@gmail.com>
2023-06-28 16:15:48 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n){
-  if(0==n){
-    return 0;
-  }else if(1==n){
-    return 1;
+  if(memo.has(n)){
+    return memo.get(n);
   }
-  return fib(n-1) + fib(n-2);
+  const value = fib(n-1) + fib(n-2);
+  memo.set(n, value);
+  
+  return value;
 }
 
 const length = 40;
@@ -13,3 +17,4 @@ const length = 40;
 for(let i=0; i<=length; i++){
   console.log(fib(i))
 }
+
