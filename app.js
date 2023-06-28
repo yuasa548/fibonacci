a7b11f491ed2ea7ff4659851bb556771b3506794
yuasa <y.kyuri2014@gmail.com>
2023-06-28 15:58:31 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+function fib(n){
+  if(0==n){
+    return 0;
+  }else if(1==n){
+    return 1;
+  }
+  return fib(n-1) + fib(n-2);
+}
+
+const length = 40;
+
+for(let i=0; i<=length; i++){
+  console.log(fib(i))
+}
